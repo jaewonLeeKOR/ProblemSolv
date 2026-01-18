d2d1393837390c9b1e819c35d17522c5c18a3d53
이재원 <ghfkddl706@me.com>
2026-01-18 17:21:53 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, s[], increaseDp[], decreaseDp[];
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        s = new int[n];
+        increaseDp = new int[n];
+        decreaseDp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            s[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            increaseDp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(s[j] < s[i]) {
+                    increaseDp[i] = Math.max(increaseDp[i], increaseDp[j] + 1);
+                }
+            }
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            decreaseDp[i] = 1;
+            for (int j = n-1; j > i; j--) {
+                if(s[j] < s[i]) {
+                    decreaseDp[i] = Math.max(decreaseDp[i], decreaseDp[j] + 1);
+                }
+            }
+        }
+
+        int m = 0;
+        for (int i = 0; i < n; i++) {
+            m = Math.max(m, increaseDp[i]+decreaseDp[i]);
+        }
+        System.out.println(m-1);
+    }
+}
